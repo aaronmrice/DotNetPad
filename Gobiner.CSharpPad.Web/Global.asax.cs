52911362ca2ad68710a751ca56809ac558f21395
Gobiner <aaron.rice.17+github@gmail.com>
2009-10-27 20:27:25 -0700

    Added ELMAH

--- Gobiner.CSharpPad.Web/Global.asax.cs
+++ Gobiner.CSharpPad.Web/Global.asax.cs
@@ -15,6 +15,7 @@ namespace Gobiner.CSharpPad.Web
 		public static void RegisterRoutes(RouteCollection routes)
 		{
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+			routes.IgnoreRoute("elmah.axd");
 
 			routes.MapRoute(
 				"ViewPaste",                                          // Route name
