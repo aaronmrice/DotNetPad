130e76fbfbe83ee5d9ec820bf0617c651abf27dc
Gobiner <aaron.rice.17+github@gmail.com>
2009-11-10 07:53:08 -0800

    Missed a file for multiple languages.

---
+++ Gobiner.CSharpPad.Web/Models/Languages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gobiner.CSharpPad.Web.Models
+{
+    public enum Language
+    {
+        CSharp,
+        VisualBasic,
+    }
+}
