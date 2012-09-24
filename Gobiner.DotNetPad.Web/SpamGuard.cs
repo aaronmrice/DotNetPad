8afa4ce70994610974a56271cb9ed2c840f35b0e
Gobiner <aaron.rice.17+github@gmail.com>
2012-09-23 21:07:50 -0700

    Removed common spam words from C# keywords. Simplified spamminess calculation.

--- Gobiner.DotNetPad.Web/SpamGuard.cs
+++ Gobiner.DotNetPad.Web/SpamGuard.cs
@@ -9,7 +9,7 @@ namespace Gobiner.DotNetPad.Web
     {
         static string[] SpammyWords = { "http" };
 
-        static string[] CSharpKeywords = { "abstract", "as", "base", "bool", "break", "byte", "case", "catch", "char", "checked", "class", "const", "continue", "decimal", "default", "delegate", "do", "double", "else", "enum", "event", "explicit", "extern", "false", "finally", "fixed", "float", "for", "foreach", "goto", "if", "implicit", "in", "in (generic modifier)", "int", "interface", "internal", "is", "lock", "long", "namespace", "new", "null", "object", "operator", "out", "out (generic modifier)", "override", "params", "private", "protected", "public", "readonly", "ref", "return", "sbyte", "sealed", "short", "sizeof", "stackalloc", "static", "string", "struct", "switch", "this", "throw", "true", "try", "typeof", "uint", "ulong", "unchecked", "unsafe", "ushort", "using", "virtual", "void", "volatile", "while" };
+        static string[] CSharpKeywords = { "abstract", "base", "bool", "break", "byte", "case", "catch", "char", "checked", "class", "const", "continue", "decimal", "default", "delegate", "double", "enum", "event", "explicit", "extern", "false", "finally", "fixed", "float", "foreach", "goto", "implicit", "int", "interface", "internal", "lock", "long", "namespace", "new", "null", "object", "operator", "out", "out (generic modifier)", "override", "params", "private", "protected", "public", "readonly", "ref", "return", "sbyte", "sealed", "short", "sizeof", "stackalloc", "static", "string", "struct", "switch", "this", "throw", "true","typeof", "uint", "ulong", "unchecked", "unsafe", "ushort", "using", "virtual", "void", "volatile", "while" };
         static string[] FSharpKeywords = { "abstract", "and", "as", "assert", "base", "begin", "class", "default", "delegate", "do", "done", "downcast", "downto", "elif", "else", "end", "exception", "extern", "false", "finally", "for", "fun", "function", "global", "if", "in", "inherit", "inline", "interface", "internal", "lazy", "let", "match", "member", "module", "mutable", "namespace", "new", "not", "null", "of", "open", "or", "override", "private", "public", "rec", "return", "static", "struct", "then", "to", "true", "try", "type", "upcast", "use", "val", "void", "when", "while", "with", "yield", "asr", "land", "lor", "lsl", "lsr", "lxor", "mod", "sig", "atomic", "break", "checked", "component", "const", "constraint", "constructor", "continue", "eager", "event", "external", "fixed", "functor", "include", "method", "mixin", "object", "parallel", "process", "protected", "pure", "sealed", "tailcall", "trait", "virtual", "volatile" };
         static string[] VisualBasicKeywords = { "AddHandler", "AddressOf", "Alias", "And", "AndAlso", "As", "Boolean", "ByRef", "Byte", "ByVal", "Call", "Case", "Catch", "CBool", "CByte", "CChar", "CDate", "CDec", "CDbl", "Char", "CInt", "Class", "CLng", "CObj", "Const", "Continue", "CSByte", "CShort", "CSng", "CStr", "CType", "CUInt", "CULng", "CUShort", "Date", "Decimal", "Declare", "Default", "Delegate", "Dim", "DirectCast", "Do", "Double", "Each", "Else", "ElseIf", "End", "EndIf", "Enum", "Erase", "Error", "Event", "Exit", "False", "Finally", "For", "Friend", "Function", "Get", "GetType", "Global", "GoSub", "GoTo", "Handles", "If", "Implements", "Imports", "In", "Inherits", "Integer", "Interface", "Is", "IsNot", "Let", "Lib", "Like", "Long", "Loop", "Me", "Mod", "Module", "MustInherit", "MustOverride", "MyBase", "MyClass", "Namespace", "Narrowing", "New", "Next", "Not", "Nothing", "NotInheritable", "NotOverridable", "Object", "Of", "On", "Operator", "Option", "Optional", "Or", "OrElse", "Overloads", "Overridable", "Overrides", "ParamArray", "Partial", "Private", "Property", "Protected", "Public", "RaiseEvent", "ReadOnly", "ReDim", "REM", "RemoveHandler", "Resume", "Return", "SByte", "Select", "Set", "Shadows", "Shared", "Short", "Single", "Static", "Step", "Stop", "String", "Structure", "Sub", "SyncLock", "Then", "Throw", "To", "True", "Try", "TryCast", "TypeOf", "Variant", "Wend", "UInteger", "ULong", "UShort", "Using", "When", "While", "Widening", "With", "WithEvents", "WriteOnly", "Xor" };
 
@@ -20,16 +20,16 @@ namespace Gobiner.DotNetPad.Web
             { Language.VisualBasic, VisualBasicKeywords },
         };
 
-        public static decimal ContentScore(Language language, string content)
+        public static int ContentScore(Language language, string content)
         {
 			if (!LanguageKeywords.ContainsKey(language)) return 0;
-            decimal keyword_count = Regex.Replace(content, "[^a-z]", " ", RegexOptions.IgnoreCase).Split(' ')
+            int keyword_count = Regex.Replace(content, "[^a-z]", " ", RegexOptions.IgnoreCase).Split(' ')
                 .Select(w => w.ToLower())
                 .Count(w => LanguageKeywords[language].Contains(w));
 
-            decimal spam_count = Regex.Matches(content, string.Join("|", SpammyWords), RegexOptions.IgnoreCase).Count + 1;
+            int spam_count = Regex.Matches(content, string.Join("|", SpammyWords), RegexOptions.IgnoreCase).Count + 1;
 
-            return keyword_count / spam_count - 1;
+            return keyword_count - spam_count;
         }
     }
 }
