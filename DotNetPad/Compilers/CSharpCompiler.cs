60dac252ee04ef74b1d3ded566406ed2b4ecc90b
Gobiner <aaron.rice.17+github@gmail.com>
2014-10-03 21:09:47 -0700

    Fixed bug where multiple classes had a static parameterless function

--- DotNetPad/Compilers/CSharpCompiler.cs
+++ DotNetPad/Compilers/CSharpCompiler.cs
@@ -7,76 +7,69 @@ using Microsoft.CSharp;
 
 namespace Gobiner.CSharpPad.Compilers
 {
-	public class CSharpCompiler : MarshalByRefObject, ICompiler
-	{
-		public string Code { get; set; }
-		public CompilerError[] Errors { get; private set; }
-		public string FileName { get; private set; }
-		public bool ProducedExecutable { get; private set; }
+    public class CSharpCompiler : MarshalByRefObject, ICompiler
+    {
+        public string Code { get; set; }
+        public CompilerError[] Errors { get; private set; }
+        public string FileName { get; private set; }
+        public bool ProducedExecutable { get; private set; }
 
-		private IDictionary<string, string> providerOptions = new Dictionary<string, string>() { { "CompilerVersion", "v4.0" } };
-		private string[] GacAssembliesToCompileAgainst = 
+        private IDictionary<string, string> providerOptions = new Dictionary<string, string>() { { "CompilerVersion", "v4.0" } };
+        private string[] GacAssembliesToCompileAgainst = 
 		{ "System.dll", "System.Core.dll", "System.Data.dll",
 		"System.Data.DataSetExtensions.dll", "Microsoft.CSharp.dll",
 		"System.Xml.dll", "System.Xml.Linq.dll", "System.Data.Entity.dll",
 		"System.Windows.Forms.dll", "System.Numerics.dll" };
 
-		public CSharpCompiler()
-		{
-			Errors = new CompilerError[] { };
-		}
+        public CSharpCompiler()
+        {
+            Errors = new CompilerError[] { };
+        }
 
-		public void Compile(string filename)
-		{
-			string mainClass = FindMainClass();
-			if (mainClass == null && Errors.Length == 0)
-			{
-				Errors = new CompilerError[] { new CompilerError(filename, 0, 0, "", "Could not find a static void Main(string[]) method") };
-				ProducedExecutable = false;
-				return;
-			}
+        public void Compile(string filename)
+        {
+            string mainClass = FindMainClass();
+            if (mainClass == null && Errors.Length == 0)
+            {
+                Errors = new CompilerError[] { new CompilerError(filename, 0, 0, "", "Could not find a static void Main(string[]) method") };
+                ProducedExecutable = false;
+                return;
+            }
 
-			var provider = new CSharpCodeProvider(providerOptions);
-			var compileParams = new CompilerParameters(GacAssembliesToCompileAgainst);
-			compileParams.MainClass = mainClass;
-			compileParams.GenerateExecutable = true;
-			compileParams.GenerateInMemory = false;
-			compileParams.OutputAssembly = filename;
+            var provider = new CSharpCodeProvider(providerOptions);
+            var compileParams = new CompilerParameters(GacAssembliesToCompileAgainst);
+            compileParams.MainClass = mainClass;
+            compileParams.GenerateExecutable = true;
+            compileParams.GenerateInMemory = false;
+            compileParams.OutputAssembly = filename;
 
-			CompilerResults r = provider.CompileAssemblyFromSource(compileParams, new string[] { this.Code });
-			Errors = r.Errors.Cast<CompilerError>().ToArray();
-			ProducedExecutable = Errors.Length == 0;
-		}
+            CompilerResults r = provider.CompileAssemblyFromSource(compileParams, new string[] { this.Code });
+            Errors = r.Errors.Cast<CompilerError>().ToArray();
+            ProducedExecutable = Errors.Length == 0;
+        }
 
-		private string FindMainClass()
-		{
-			var provider = new CSharpCodeProvider(providerOptions);
-			var compileParams = new CompilerParameters(GacAssembliesToCompileAgainst);
-			compileParams.GenerateExecutable = false;
-			compileParams.GenerateInMemory = true;
+        private string FindMainClass()
+        {
+            var provider = new CSharpCodeProvider(providerOptions);
+            var compileParams = new CompilerParameters(GacAssembliesToCompileAgainst);
+            compileParams.GenerateExecutable = false;
+            compileParams.GenerateInMemory = true;
 
-			CompilerResults r = provider.CompileAssemblyFromSource(compileParams, new string[] { this.Code });
+            CompilerResults r = provider.CompileAssemblyFromSource(compileParams, new string[] { this.Code });
 
-			Errors = r.Errors.Cast<CompilerError>().ToArray();
+            Errors = r.Errors.Cast<CompilerError>().ToArray();
 
-			if (Errors.Count() > 0)
-				return null;
+            if (Errors.Count() > 0)
+                return null;
 
-			var allTypes = r.CompiledAssembly.GetTypes();
-			foreach (var type in allTypes)
-			{
-				var possibleMethods = type.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
-				foreach (var mainMethod in possibleMethods)
-				{
-					var parameters = mainMethod.GetParameters();
-					if (parameters.Length == 0 ||
-					   (parameters.Length == 1 && parameters[0].ParameterType == typeof(string[])))
-					{
-						return type.FullName;
-					}
-				}
-			}
-			return null;
-		}
-	}
+            var mainMethods = r.CompiledAssembly.GetTypes()
+                              .SelectMany(x => x.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic))
+                              .Where(x => x.Name == "Main")
+                              .Where(x => x.GetParameters().Length == 0 || (x.GetParameters().Length == 1 && x.GetParameters()[0].ParameterType == typeof(string[])));
+
+            if (!mainMethods.Any()) return null;
+
+            return mainMethods.First().DeclaringType.FullName;
+        }
+    }
 }
